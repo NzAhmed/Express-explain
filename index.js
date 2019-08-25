e375887397060e77c0458298b88f29884e6c10a0
NzAhmed <33981320+NzAhmed@users.noreply.github.com>
2019-08-25 15:16:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ var express = require("express");
 
 var app = express();
 
+
+app.get('/', function (req, res) {
+  res.send('hello world')
+})
+
 app.listen(3000, function(){
 	console.log("server now listening!");
 });
+
+
+// https://flaviocopes.com/express/
 
